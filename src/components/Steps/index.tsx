4e51fc0d0561face911de4f9d5ad55b23ce244d8
Joao Teles <joaojoseteles@gmail.com>
2023-01-16 23:05:17 -0300

    :recycle: refactor: components Steps com styles

--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -1,10 +1,41 @@
+/* Styles */
+import styles from "./Steps.module.scss";
+import classnames from "classnames";
+
+/* Components */
+import { AiOutlineUser, AiOutlineStar } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
+
 type stepsProps = {
   currentStep: number;
 };
 export default function Steps({ currentStep }: stepsProps) {
   return (
     <>
-      <span>{currentStep}</span>
+      <div className={styles.steps}>
+        <div className={`${styles.step} ${styles.active}`}>
+          <AiOutlineUser />
+          <p>Identificação</p>
+        </div>
+        <div
+          className={classnames(
+            styles.step,
+            currentStep >= 1 ? styles.active : ""
+          )}
+        >
+          <AiOutlineStar />
+          <p>Avaliação</p>
+        </div>
+        <div
+          className={classnames(
+            styles.step,
+            currentStep >= 2 ? styles.active : ""
+          )}
+        >
+          <FiSend />
+          <p>Envio</p>
+        </div>
+      </div>
     </>
   );
 }
