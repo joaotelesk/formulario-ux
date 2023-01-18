35653ec607f055fdd570ef78129064991e62978c
Joao Teles <joaojoseteles@gmail.com>
2023-01-18 18:36:12 -0300

    :recycle: refactor: components com elementos e estilos

--- src/components/ThanksForm/index.tsx
+++ src/components/ThanksForm/index.tsx
@@ -1,7 +1,38 @@
-export default function ThanksForm() {
+type UserProps = {
+  data: {
+    name: string;
+    birth: string;
+    city: string;
+    quest1: string;
+    quest2: string;
+    quest3: string;
+    quest4: string;
+  };
+};
+
+export default function ThanksForm({ data }: UserProps) {
   return (
     <>
-      <h1>ThanksForm</h1>
+      <div className="flex-colum desktop:w-96 mobile:w-64">
+        <h2 className="mt-2 mb-2">Falta pouco...</h2>
+        <p>A sua opinião é muito importante.</p>
+        <p>Para concluir clique no botão de Enviar abaixo.</p>
+        <h3 className="text-start">Aqui está o resumo da sua avaliação:</h3>
+        <div className="text-start mt-4">
+          <span>
+            <p>Nome: {data.name}</p>
+            <p>Idade: {data.birth}</p>
+            <p>Cidade:{data.city}</p>
+          </span>
+
+          <span>
+            <p>Questão 1: {data.quest1}</p>
+            <p>Questão 2: {data.quest2}</p>
+            <p>Questão 3: {data.quest3}</p>
+            <p>Questão 4: {data.quest4}</p>
+          </span>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/UserForm/index.tsx
+++ src/components/UserForm/index.tsx
@@ -1,7 +1,61 @@
-export default function UserForm() {
+type UserProps = {
+  data: {
+    name: string;
+    birth: string;
+    city: string;
+  };
+  updateFieldHandler: (key: string, value: string) => void;
+};
+
+export default function UserForm({ data, updateFieldHandler }: UserProps) {
   return (
     <>
-      <h1>UserForm</h1>
+      <div className="flex-colum text-start desktop:w-96 mobile:w-64">
+        <label htmlFor="name" className="">
+          Nome:
+        </label>
+        <input
+          type="text"
+          name="nome"
+          id="name"
+          placeholder="Ex.: Renata"
+          className="input"
+          required
+          value={data.name}
+          minLength={3}
+          onChange={(e) => updateFieldHandler("name", e.target.value)}
+        />
+        <div className="flex text-start gap-3 mt-6">
+          <div>
+            <label htmlFor="birth">Aniversário</label>
+            <input
+              type="date"
+              name="birth"
+              id="birth"
+              className="input"
+              required
+              min="1930-01-01"
+              max="2002-12-31"
+              value={data.birth || ""}
+              onChange={(e) => updateFieldHandler("birth", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="city">Cidade/Estado</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              className="input"
+              placeholder="Ex.: Fortaleza/CE"
+              required
+              min={3}
+              value={data.city || ""}
+              onChange={(e) => updateFieldHandler("city", e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
